2285d146e7a35a33b61890d4166d45831fc94aac
BichevoyIgor <igoreek@mail.ru>
2021-05-24 10:56:31 +0300

    base code+

--- src/main/java/com/bichevoy/nio/NioTelnetServer.java
+++ src/main/java/com/bichevoy/nio/NioTelnetServer.java
@@ -4,18 +4,27 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NioTelnetServer {
     public static final String LS_COMMAND = "\tls   view all files and directories\n";
     public static final String MKDIR_COMMAND = "\tmkdir   create directory\n";
     public static final String CHANGE_NICKNAME = "\tnick   change nickname\n";
+    private static final String ROOT_NOTIFICATION = "You are alredy in the root directory\n\n";
+    private static final String ROOT_PATH = "server";
+    private static final String DIRECTORY_DOESNT_EXIST = "Directory %s doesn't exist\n\n";
 
+    private Path currentPath = Path.of("server");
+    private Map<SocketAddress, String> clients = new HashMap<>();
     private final ByteBuffer buffer = ByteBuffer.allocate(512);
 
     public NioTelnetServer() throws IOException {
@@ -46,6 +55,8 @@ public class NioTelnetServer {
     }
 
     private void handleRead(SelectionKey key, Selector selector) throws IOException {
+        String nickName = "";
+
         SocketChannel channel = ((SocketChannel) key.channel());
         SocketAddress client = channel.getRemoteAddress();
         int readBytes = channel.read(buffer);
@@ -79,17 +90,50 @@ public class NioTelnetServer {
                 sendMessage(CHANGE_NICKNAME, selector, client);
             }else if ("ls".equals(command)){
                 sendMessage(getFileList().concat("\n"), selector, client);
-            }
-            else if ("exit".equals(command)){
+            }else if (command.startsWith("nick ")){
+                nickName = command.split(" ")[1];
+                clients.put(channel.getRemoteAddress(), nickName);
+                System.out.println("Client - " + channel.getRemoteAddress().toString() + "changed nickname on " + nickName);
+                System.out.println(clients);
+            }else if (command.startsWith("cd ")){
+                String neededPathString = command.split(" ")[1];
+                Path tempPath = Path.of(currentPath.toString(), neededPathString);
+                if ("..".equals(neededPathString)){
+                    tempPath = currentPath.getParent();
+                    if (tempPath == null || !tempPath.toString().startsWith("server")){
+                        sendMessage(ROOT_NOTIFICATION, selector,client);
+                    }else {
+                        currentPath = tempPath;
+                    }
+                }else if ("~".equals(neededPathString)) {
+                    currentPath = Path.of(ROOT_PATH);
+                }else {
+                    if(tempPath.toFile().exists()){
+                        currentPath = tempPath;
+                    }else {
+                        sendMessage(String.format(DIRECTORY_DOESNT_EXIST, neededPathString), selector, client);
+                    }
+                }
+            } else if ("exit".equals(command)){
                 System.out.println("Client logged out. IP: " + channel.getRemoteAddress());
                 channel.close();
                 return;
             }
         }
+        sendName(channel, nickName);
+    }
+
+    private void sendName(SocketChannel channel, String nickName) throws IOException {
+        if (nickName.isEmpty()){
+            nickName = clients.getOrDefault(channel.getRemoteAddress(), channel.getRemoteAddress().toString());
+        }
+        String curentPathString = currentPath.toString().replace("server", "~");
+        channel.write(ByteBuffer.wrap(nickName.concat(">:").concat(curentPathString).concat("$ ").getBytes(StandardCharsets.UTF_8)
+        ));
     }
 
     private String getFileList() {
-        return String.join(" ", new File("server").list());
+        return String.join(" ", new File(currentPath.toString()).list());
     }
 
     private void sendMessage(String message, Selector selector, SocketAddress client) throws IOException {
@@ -111,6 +155,7 @@ public class NioTelnetServer {
         channel.register(selector, SelectionKey.OP_READ, "some attach");
         channel.write(ByteBuffer.wrap("Hello user\n".getBytes(StandardCharsets.UTF_8)));
         channel.write(ByteBuffer.wrap("Enter -- help for support info\n".getBytes(StandardCharsets.UTF_8)));
+        sendName(channel, "");
 
     }
 
